9e51dd7db0abf2720ee157b13ec14ffa7ebf8a12
lishunxiang <lishunxiang@vip.163.com>
2018-11-21 23:26:37 +0800

    监控界面设置安全登录验证

--- src/main/java/com/sxli/druiddemo/servlet/DruidStatViewServlet.java
+++ src/main/java/com/sxli/druiddemo/servlet/DruidStatViewServlet.java
@@ -2,8 +2,21 @@ package com.sxli.druiddemo.servlet;
 
 import com.alibaba.druid.support.http.StatViewServlet;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(urlPatterns = "/druid/*")
 public class DruidStatViewServlet extends StatViewServlet {
+
+    /**
+     * 重写初始化方法并且定义监控界面登录密码
+     *
+     * @throws ServletException
+     */
+    @Override
+    public void init() throws ServletException {
+        super.username = "sxli";
+        super.password = "sxli";
+        super.init();
+    }
 }
