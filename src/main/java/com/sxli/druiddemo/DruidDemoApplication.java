43e04174b0d23a0fe1e86685c33e03e8a5d95c94
lishunxiang <lishunxiang@vip.163.com>
2018-11-21 23:16:47 +0800

    Druid开启监控界面

--- src/main/java/com/sxli/druiddemo/DruidDemoApplication.java
+++ src/main/java/com/sxli/druiddemo/DruidDemoApplication.java
@@ -3,9 +3,12 @@ package com.sxli.druiddemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.sxli.druiddemo.dao")
+// 扫描Servlet
+@ServletComponentScan
 public class DruidDemoApplication {
 
     public static void main(String[] args) {
